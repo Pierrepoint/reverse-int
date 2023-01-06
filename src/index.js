766a8ba1bd80e643d2e1ac79210a900191cdb949
Orlando <drapezoid@gmail.com>
2023-01-06 03:54:36 +0300

    Update: index.js fully correct implementation done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+newN = String(n);
+if (newN[newN.length - 1] == '0'){newN = newN.slice(0,newN.length - 1);};
+if (newN[0] == '-'){newN = newN.slice(1);};
+let a = newN.length - 1;
+let b = "";
+while (a >= 0) {
+b = b + newN[a];
+a = a - 1;
+};
+return b;
 }
